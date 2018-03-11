4e7d894030b353085e384ad1881a8b6b9b71e148
Timothy Landwehr <tjlandwehr@gmail.com>
2018-03-10 18:16:47 -0600

    Added search and list functionality

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,32 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("all") && searchTerm != null)
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search for " + searchTerm;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            else if (searchType.Equals("all") && searchTerm == null)
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search for " + searchTerm;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search for " + searchTerm;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+        }
     }
 }
